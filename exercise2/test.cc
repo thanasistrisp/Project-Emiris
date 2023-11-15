f4f73be786b491f5d72124d8f88ce47358a5b769
sdi2000026 <sdi2000026@di.uoa.gr>
2023-11-15 23:30:50 +0200

    Add get_successors(index, E), update tests

--- exercise2/test.cc
+++ exercise2/test.cc
@@ -11,15 +11,18 @@ int main()
     Vertex v10(0, 2);
     Vertex v01(1, 3.0);
     Vertex v02(2, 1.2);
+    Vertex v03(3, 0.2);
+    Vertex v04(4, 2);
     Vertex v12(1, 0.3);
     g.add_vertex(0);
     g.add_edge(0, v01);
     g.add_edge(0, v02);
+    g.add_edge(0, v03);
     g.add_edge(1, v10);
     g.add_edge(1, v12);
     g.add_vertex(3);
     cout << "Sucessors of vertex with index 0" << endl;
-    vector<Vertex*> s = g.get_successors(0);
+    vector<Vertex*> s = g.get_successors(0, 2);
     for(int i = 0; i < s.size(); i++){
         cout << s[i]->get_index() << " " << s[i]->get_distance() << endl;
     }
