6a77360859b0e6ce2bd117889cdf2306b167b84f
sdi2000026 <sdi2000026@di.uoa.gr>
2023-11-18 19:23:07 +0200

    Remove Vertex from all files - tested - removed vertex.cc, vertex.hpp - changed interface of DirectedGraph class (again) - reduced memory footprint

--- exercise2/include/directed_graph.hpp
+++ exercise2/include/directed_graph.hpp
@@ -2,12 +2,10 @@
 
 #include <vector>
 
-#include "vertex.hpp"
-
 class DirectedGraph
 {
     private:
-        std::vector<std::vector<Vertex*>> adjacency_lists; 
+        std::vector<std::vector<int>> adjacency_lists;
 
     public:
         DirectedGraph();
@@ -15,11 +13,11 @@ class DirectedGraph
 
         void add_vertex(int);
 
-        void add_edge(int, int, double);
-        void add_edge(int, const std::vector<int>&, const std::vector<double>&);
+        void add_edge(int, int);
+        void add_edge(int, const std::vector<int>&);
 
-        std::tuple<std::vector<int>, std::vector<double>> get_successors(int) const;
-        std::tuple<std::vector<int>, std::vector<double>> get_successors(int, int) const;
+        std::vector<int> get_successors(int) const;
+        std::vector<int> get_successors(int, int) const;
 
         std::vector<int> get_predecessors(int) const;
         std::vector<int> get_predecessors(int, int) const;
