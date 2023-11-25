ebb63f13b344eed70e1e64a4d07487c11c4d72fd
sdi2000026 <sdi2000026@di.uoa.gr>
2023-11-25 15:35:01 +0200

    Fix bug in LSH - remove duplicates

--- exercise1/A/LSH/lsh.cc
+++ exercise1/A/LSH/lsh.cc
@@ -2,6 +2,7 @@
 #include <iterator>
 #include <tuple>
 #include <set>
+#include <unordered_set>
 // iterator is used for std::const_iterator, std::advance().
 
 #include "lsh.hpp"
@@ -57,6 +58,7 @@ tuple<vector<int>, vector<double>> LSH::query(const vector<double>& q, unsigned 
 {
     auto compare = [](tuple<int, double> t1, tuple<int, double> t2){ return get<1>(t1) < get<1>(t2); };
     multiset<tuple<int, double>, decltype(compare)> s(compare);
+    unordered_set<int> unique_indices;
 
     double dist;
     int p_index;
@@ -95,7 +97,10 @@ tuple<vector<int>, vector<double>> LSH::query(const vector<double>& q, unsigned 
                 }
                 s.erase(std::prev(s.end()));
             }
-            s.insert(make_tuple(p_index, dist));
+            if(unique_indices.find(p_index) == unique_indices.end()){
+                s.insert(make_tuple(p_index, dist));
+                unique_indices.insert(p_index);
+            }
         }
     }
 
