c0b6189b3fe20234a2e4def6213dd893d2defc78
Thanasis Trispiotis <thanasistrisp@gmail.com>
2023-11-28 23:39:32 +0200

    change name

--- exercise2/script.sh
+++ exercise2/script.sh
@@ -1,7 +1,7 @@
-./graphsearch -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 1000 -m 3 -out 30
-./graphsearch -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 500 -m 3 -out 30
-./graphsearch -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 300 -m 3 -out 20
-./graphsearch -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 1000 -m 3 -out 100
-./graphsearch -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 500 -m 3 -out 200
-./graphsearch -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 100 -m 3 -out 50
-./graphsearch -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 100 -m 3 -out 20
+./graph_search -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 1000 -m 3 -out 30
+./graph_search -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 500 -m 3 -out 30
+./graph_search -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 300 -m 3 -out 20
+./graph_search -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 1000 -m 3 -out 100
+./graph_search -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 500 -m 3 -out 200
+./graph_search -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 100 -m 3 -out 50
+./graph_search -d MNIST/input.dat -o output/output.txt -q MNIST/query.dat -k 50 -E 30 -R 1 -N 5 -l 100 -m 3 -out 20
