a0462d4b7fb52545b5eddd6222973514b5768992
Thanasis Trispiotis <thanasistrisp@gmail.com>
2023-12-26 13:31:45 +0200

    flatten, deflatten, encode, decode to binary - reduce.py produces correct binaries for reduced dimension - no need for giving model type (conv or dense), we check output layer shape

--- exercise3/helper_funcs.py
+++ exercise3/helper_funcs.py
@@ -6,22 +6,24 @@ from tensorflow import keras
 
 def save_encoded_binary(encoded_imgs, output_file):
 	with open(output_file, 'wb') as f:
-		# get latent dimension
-		NEW_DIM = encoded_imgs.shape[1] # case dense
-		if encoded_imgs.ndim == 4:      # case conv
-			NEW_DIM *= (encoded_imgs.shape[2] * encoded_imgs.shape[3])
+		# flatten encoded images
+		encoded_imgs = flatten_encoded(encoded_imgs)
 		# write magic number
 		f.write((0).to_bytes(4, byteorder='big'))
 		# write number of images
 		f.write((len(encoded_imgs)).to_bytes(4, byteorder='big'))
 		# write number of rows
-		f.write((NEW_DIM).to_bytes(4, byteorder='big'))
-		# write number of columns
 		f.write((1).to_bytes(4, byteorder='big'))
+		# write number of columns
+		f.write((encoded_imgs.shape[1]).to_bytes(4, byteorder='big'))
 		# write data
-		for i in range(len(encoded_imgs)):
-			for j in range(NEW_DIM):
-				f.write(encoded_imgs[i][j].astype('float32').tobytes())
+		f.write(encoded_imgs.astype('float32').tobytes())
+
+def flatten_encoded(encoded_imgs):
+	return encoded_imgs.reshape(len(encoded_imgs), np.prod(encoded_imgs.shape[1:]))
+
+def deflatten_encoded(encoded_imgs, shape):
+	return encoded_imgs.reshape(encoded_imgs.shape[0], *shape)
 
 def save_decoded_binary(decoded_imgs, output_file):
 	with open(output_file, 'wb') as f:
@@ -34,20 +36,17 @@ def save_decoded_binary(decoded_imgs, output_file):
 		# write number of columns
 		f.write((28).to_bytes(4, byteorder='big'))
 		# write data
-		for i in range(len(decoded_imgs)):
-			for j in range(28):
-				for k in range(28):
-					f.write(decoded_imgs[i][j * 28 + k].astype('float32').tobytes())
+		f.write(decoded_imgs.astype('float32').tobytes())
 
-def load_dataset(dataset):
+def load_dataset(dataset, dtype=np.uint8):
 	with open(dataset, 'rb') as f:
 		magic_number = int.from_bytes(f.read(4), byteorder='big')
 		num_images = int.from_bytes(f.read(4), byteorder='big')
 		rows = int.from_bytes(f.read(4), byteorder='big')
 		cols = int.from_bytes(f.read(4), byteorder='big')
-		data = np.fromfile(f, dtype=np.uint8)
+		data = np.fromfile(f, dtype=dtype)
 		data = data.reshape(num_images, rows, cols)
-		return data
+	return data
 
 def print_digits(decoded_imgs, x_test):
     n = 10  # How many digits we will display
--- exercise3/reduce.py
+++ exercise3/reduce.py
@@ -21,10 +21,9 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument('-d', '--dataset', help='dataset', type=str, default='MNIST/input.dat')
 parser.add_argument('-q', '--query', help='queryset', type=str, default='MNIST/query.dat')
-parser.add_argument('-od', '--output_dataset', help='output_dataset_file', type=str, default='output.dat')
-parser.add_argument('-oq', '--output_query', help='output_query_file', type=str, default='query.dat')
+parser.add_argument('-od', '--output_dataset', help='output_dataset_file', type=str, default='MNIST/output_dataset.dat')
+parser.add_argument('-oq', '--output_query', help='output_query_file', type=str, default='MNIST/output_query.dat')
 parser.add_argument('-m', '--model', help='model_file', type=str, default='models/model_conv_12.keras')
-parser.add_argument('-t', '--type', help='model_type', type=str, default='conv')
 
 args = parser.parse_args()
 dataset = args.dataset
@@ -35,13 +34,14 @@ model = args.model
 
 # load model
 autoencoder = load_model(model)
+shape = autoencoder.layers[-2].output_shape[1:] # get shape of encoded layer
 
 # load dataset
 x_train = load_dataset(dataset)
 x_train = x_train.astype('float32') / 255.
 x_test = load_dataset(query)
 x_test = x_test.astype('float32') / 255.
-if args.type == 'conv':
+if len(shape) == 3: # if model type is convolutional
 	x_train = np.reshape(x_train, (len(x_train), 28, 28, 1))
 	x_test = np.reshape(x_test, (len(x_test), 28, 28, 1))
 else:
@@ -51,8 +51,20 @@ else:
 encoded_train = autoencoder.encode(x_train)
 encoded_test = autoencoder.encode(x_test)
 
-decoded_train = autoencoder.decode(encoded_train)
-# decoded_train = autoencoder.predict(x_train) # same as above
-decoded_test = autoencoder.decode(encoded_test)
+# save encoded datasets
+save_encoded_binary(encoded_train, output_dataset)
+save_encoded_binary(encoded_test, output_query)
 
-print_digits(decoded_train, x_train)
+# # load encoded datasets (already normalized)
+# encoded_train = load_dataset(output_dataset, dtype=np.float32)
+# encoded_test = load_dataset(output_query, dtype=np.float32)
+
+# # deflatten encoded datasets
+# encoded_train = deflatten_encoded(encoded_train, shape)
+# encoded_test = deflatten_encoded(encoded_test, shape)
+
+# # decode encoded datasets
+# decoded_train = autoencoder.decode(encoded_train)
+# decoded_test = autoencoder.decode(encoded_test)
+
+# print_digits(decoded_train, x_train)
