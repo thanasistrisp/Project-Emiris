1a1390d13fdf2ec934b6d51a995e3e4c25562991
Thanasis Trispiotis <thanasistrisp@gmail.com>
2023-12-29 00:27:49 +0200

    add load_file as optional parameter

--- exercise3/params.py
+++ exercise3/params.py
@@ -20,27 +20,22 @@ def hypercube_test(input, query, queries_num, k, M, probes, N):
     lib.get_hypercube_results(input, query, queries_num, k, M, probes, N, ctypes.byref(average_time), ctypes.byref(maf))
     return average_time, maf
 
-def gnn_test(input, query, queries_num, k, E, R, N):
+def gnn_test(input, query, queries_num, k, E, R, N, load_file=b''):
     lib.get_gnn_results.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_char_p, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
-    # load_file = b'../graph_files/gnns21.bin'
-    load_file = b''
     average_time = ctypes.c_double()
     maf = ctypes.c_double()
     lib.get_gnn_results(input, query, queries_num, k, E, R, N, load_file, ctypes.byref(average_time), ctypes.byref(maf))
     return average_time, maf
 
-def mrng_test(input, query, queries_num, l, N):
+def mrng_test(input, query, queries_num, l, N, load_file=b''):
     lib.get_mrng_results.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_char_p, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
-    # load_file = b'../graph_files/mrng_graph.bin'
-    load_file = b''
     average_time = ctypes.c_double()
     maf = ctypes.c_double()
     lib.get_mrng_results(input, query, queries_num, l, N, load_file, ctypes.byref(average_time), ctypes.byref(maf))
     return average_time, maf
 
-def nsg_test(input, query, queries_num, m, l, lq, k, N):
+def nsg_test(input, query, queries_num, m, l, lq, k, N, load_file=b''):
     lib.get_nsg_results.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_char_p, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
-    load_file = b''
     average_time = ctypes.c_double()
     maf = ctypes.c_double()
     lib.get_nsg_results(input, query, queries_num, m, l, lq, k, N, load_file, ctypes.byref(average_time), ctypes.byref(maf))
