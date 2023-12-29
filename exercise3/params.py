39e840657c8c773601edc636ff11111af52cd304
Eleftheria Vrachoriti <sdi2000026@di.uoa.gr>
2023-12-29 09:55:50 +0200

    Change maf to aaf in exercise3/params.py

--- exercise3/params.py
+++ exercise3/params.py
@@ -8,38 +8,38 @@ lib = ctypes.CDLL(libname)
 def lsh_test(input, query, queries_num, k, L, table_size, window_size, query_trick, N):
     lib.get_lsh_results.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_bool, ctypes.c_int, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_int))
     average_time = ctypes.c_double()
-    maf = ctypes.c_double()
+    aaf = ctypes.c_double()
     min_neighbors = ctypes.c_int()
-    lib.get_lsh_results(input, query, queries_num, k, L, table_size, window_size, query_trick, N, ctypes.byref(average_time), ctypes.byref(maf), ctypes.byref(min_neighbors))
-    return average_time, maf, min_neighbors
+    lib.get_lsh_results(input, query, queries_num, k, L, table_size, window_size, query_trick, N, ctypes.byref(average_time), ctypes.byref(aaf), ctypes.byref(min_neighbors))
+    return average_time, aaf, min_neighbors
 
 def hypercube_test(input, query, queries_num, k, M, probes, N):
     lib.get_hypercube_results.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
     average_time = ctypes.c_double()
-    maf = ctypes.c_double()
-    lib.get_hypercube_results(input, query, queries_num, k, M, probes, N, ctypes.byref(average_time), ctypes.byref(maf))
-    return average_time, maf
+    aaf = ctypes.c_double()
+    lib.get_hypercube_results(input, query, queries_num, k, M, probes, N, ctypes.byref(average_time), ctypes.byref(aaf))
+    return average_time, aaf
 
 def gnn_test(input, query, queries_num, k, E, R, N, load_file=b''):
     lib.get_gnn_results.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_char_p, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
     average_time = ctypes.c_double()
-    maf = ctypes.c_double()
-    lib.get_gnn_results(input, query, queries_num, k, E, R, N, load_file, ctypes.byref(average_time), ctypes.byref(maf))
-    return average_time, maf
+    aaf = ctypes.c_double()
+    lib.get_gnn_results(input, query, queries_num, k, E, R, N, load_file, ctypes.byref(average_time), ctypes.byref(aaf))
+    return average_time, aaf
 
 def mrng_test(input, query, queries_num, l, N, load_file=b''):
     lib.get_mrng_results.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_char_p, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
     average_time = ctypes.c_double()
-    maf = ctypes.c_double()
-    lib.get_mrng_results(input, query, queries_num, l, N, load_file, ctypes.byref(average_time), ctypes.byref(maf))
-    return average_time, maf
+    aaf = ctypes.c_double()
+    lib.get_mrng_results(input, query, queries_num, l, N, load_file, ctypes.byref(average_time), ctypes.byref(aaf))
+    return average_time, aaf
 
 def nsg_test(input, query, queries_num, m, l, lq, k, N, load_file=b''):
     lib.get_nsg_results.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_int, ctypes.c_char_p, ctypes.POINTER(ctypes.c_double), ctypes.POINTER(ctypes.c_double))
     average_time = ctypes.c_double()
-    maf = ctypes.c_double()
-    lib.get_nsg_results(input, query, queries_num, m, l, lq, k, N, load_file, ctypes.byref(average_time), ctypes.byref(maf))
-    return average_time, maf
+    aaf = ctypes.c_double()
+    lib.get_nsg_results(input, query, queries_num, m, l, lq, k, N, load_file, ctypes.byref(average_time), ctypes.byref(aaf))
+    return average_time, aaf
 
 class CA(Structure):
     _fields_ = [('model', c_char_p),
